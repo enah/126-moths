a80062f533fe8afa83cf451a2ea0128141fb19a4
Ajeya <ajeyac@berkeley.edu>
2014-10-23 07:10:02 -0700

    Changed sim.py to return fraction of grays, almost done with writeup

--- sim.py
+++ sim.py
@@ -1,32 +1,63 @@
 import random
 
-def simulate(initial_state, prob_white_death=0.8, max_steps=10000):
+def simulate(initial_state, prob_white=0.9, max_steps=10000):
     """
-    Returns the number of time steps it takes to get absorbed 
-    into the state where all moths are gg; if it instead gets
-    absorbed into the state where all moths are ww, it returns False
+    Returns True if white moths are completely eliminated within max_steps;
+    False if gray moths are eliminated; fraction of gray moths if no absorbing
+    state is reached.
+
+    initial_state: A dictionary mapping genotypes (strings) to initial 
+        numbers of moths of that genotype.
+    prob_white: The probability that a white moth is killed during
+        the state transition phase.
+    max_steps: The maximum allowed time steps.
+
     """
     pop_size = sum([initial_state[genotype] for genotype in initial_state])
    
     curr_state = initial_state
     for step in range(1, max_steps + 1):
-        curr_state = transition(curr_state, pop_size, prob_white_death)
+        curr_state = transition(curr_state, pop_size, prob_white)
         if all_gg(curr_state):
-            return step
+            return True # Eliminated white moths
         elif all_ww(curr_state):
-            return False
-    return False
+            return False 
+    return curr_state['gg'] / pop_size 
+
+def transition(curr_state, pop_size, prob_white):
+    """
+    Takes in a current state and returns a new state 
+    after one moth has been born and one has died.
+
+    p_w: The probability that a white moth is killed in 
+    the death phase.
+    
+    """
+    new_state = dict(curr_state)
+    child = new_child(curr_state, pop_size)
+    new_state[child] += 1
+
+    dead_moth = kill_moth(new_state, prob_white)
+    new_state[dead_moth] -= 1
+
+    return new_state 
 
 def all_gg(state):
+    """
+    Returns True if the white allele has been eliminated.
+    """
     return state['wg'] == 0 and state['ww'] == 0
 
 def all_ww(state):
+    """
+    Returns True if the gray allele has been eliminated.
+    """
     return state['wg'] == 0 and state['gg'] == 0
 
 def random_moth(state, pop_size):
     """
-    Takes in a state and returns a string in ('ww', 'wg', 'gg')
-    that represents a randomly chosen moth from that population.
+    Returns a string in {'ww', 'wg', 'gg'}, representing a 
+    random moth from the population in state.
     """
     x = random.randint(0, pop_size - 1)
     if x < state['ww']:
@@ -37,8 +68,8 @@ def random_moth(state, pop_size):
 
 def new_child(state, pop_size):
     """
-    Takes in a state and returns a string in ('ww', 'wg', 'gg') that
-    represents a child born to two randomly chosen parents from that population.
+    Returns a string in {'ww', 'wg', 'gg'} representing 
+    a child born to two random parents from the population in state.
     """
     parent1, parent2 = random_moth(state, pop_size), random_moth(state, pop_size)
     child = random.choice(parent1) + random.choice(parent2)
@@ -48,9 +79,8 @@ def new_child(state, pop_size):
 
 def kill_moth(state, prob_white):
     """
-    Takes in a state and returns a string in ('ww', 'wg', 'gg') that
-    represents a moth to be killed. White moths are killed with 
-    prob_white, gray moths with 1 - prob_white.
+    Returns a string in {'ww', 'wg', 'gg'} representing a moth to kill. 
+    Moth is white with prob_white, gray with 1 - prob_white.
     """
     if state['gg'] == 0 or random.random() < prob_white:
         total_white = state['ww'] + state['wg']
@@ -58,17 +88,3 @@ def kill_moth(state, prob_white):
             return 'ww'
         return 'wg'
     return 'gg'
-
-def transition(curr_state, pop_size, p_w):
-    """
-    Takes in a current state and returns a new state 
-    after one moth has been born and one has died.
-    """
-    new_state = dict(curr_state)
-    child = new_child(curr_state, pop_size)
-    new_state[child] += 1
-
-    dead_moth = kill_moth(new_state, p_w)
-    new_state[dead_moth] -= 1
-
-    return new_state 
